96142a8672e721d5ae5bcf4625f4a168ae385182
Harsh Kumar <harshofficial1905@gmail.com>
2025-10-31 20:49:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,17 +1,24 @@
 class Solution {
     ArrayList<Integer> ans = new ArrayList<>();
-    public void dfs(boolean visited[], List<List<Integer>> list,int i){
+    public void bfs(int incoming[] ,boolean visited[],List<List<Integer>> list, int i){
         visited[i] = true;
-        for(int j=0;j<list.get(i).size();j++){
-            int curr = list.get(i).get(j);
-            if(!visited[curr]){
-                dfs(visited,list,curr);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        while(!q.isEmpty()){
+            int top = q.remove();
+            ans.add(top);
+            for(int j=0;j<list.get(top).size();j++){
+                int curr = list.get(top).get(j);
+                incoming[curr]--;
+                if(incoming[curr]==0) {
+                    q.add(curr);
+                    visited[curr] =true;
+                }
             }
         }
-        ans.add(i);
     }
-    public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        List<List<Integer>> list = new ArrayList<>();
+     public ArrayList<Integer> topoSort(int V, int[][] edges) {
+         List<List<Integer>> list = new ArrayList<>();
         for(int i=0;i<V;i++){
             list.add(new ArrayList<>());
         }
@@ -19,13 +26,19 @@ class Solution {
             int a = edges[i][0]; int b = edges[i][1];
             list.get(a).add(b);
         }
-        boolean visited[]  = new boolean[list.size()];
-        for(int i=0;i<list.size();i++){
-            if(visited[i] == false){
-                dfs(visited,list,i);
+        boolean visited[]  = new boolean[V];
+        
+        int incoming[] = new int[V];
+        for(int i=0;i<edges.length;i++){
+             int x = edges[i][1];
+            incoming[x]++;
+        }
+        
+        for(int i=0;i<V;i++){
+            if(visited[i] == false && incoming[i]==0){
+                bfs(incoming,visited,list,i);
             }
         }
-        Collections.reverse(ans);
         return ans;
     }
 }
